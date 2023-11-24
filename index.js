d40af05b380fb39b62d1044022e51ed92d9766fe
krishna-200 <20mh1a4242@acoe.edu.in>
2023-11-24 21:03:13 +0530

    backend

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/user");
+const postsRoute = require("./routes/posts");
+const commentsRoute = require("./routes/comments");
+const path = require("path");
+const cors = require("cors");
+const multer = require("multer");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+const mongodb = async () => {
+  try {
+    await mongoose.connect(process.env.mongo_url);
+    console.log("DB is connectd");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+dotenv.config();
+app.use(express.json());
+app.use(cookieParser());
+app.use("/images", express.static(path.join(__dirname, "/images")));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
+app.use("/api/posts", postsRoute);
+app.use("/api/comments", commentsRoute);
+
+//Image upload
+const storage = multer.diskStorage({
+  destination: (req, file, fn) => {
+    fn(null, "images");
+  },
+  filename: (req, file, fn) => {
+    fn(null, req.body.img);
+    // fn(null, "image1.png");
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("Image has been uploaded successfully!");
+});
+
+app.listen(process.env.PORT, () => {
+  mongodb();
+  console.log(`app is up and running in port ${process.env.PORT}`);
+});
+
+//m8MWi7SYpsCclST5
---
+++ routes/posts.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const Post = require("../models/Post");
+const Comment = require("../models/Comment");
+const verifyToken = require("../verifyToken");
+
+router.post("/create", verifyToken, async (req, res) => {
+  try {
+    const newPost = new Post(req.body);
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.put("/:id", verifyToken, async (req, res) => {
+  try {
+    const updatedUser = await Post.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+router.delete("/:id", verifyToken, async (req, res) => {
+  try {
+    await Post.findByIdAndDelete(req.params.id);
+    await Comment.deleteMany({ postId: req.params.id });
+    res.status(200).json("post has been deleted");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/", async (req, res) => {
+  const query = req.query;
+  try {
+    const searchFilter = {
+      title: { $regex: query.search, $options: "i" },
+    };
+    const posts = await Post.find(query.search ? searchFilter : null);
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/user/:userId", async (req, res) => {
+  try {
+    const posts = await Post.find({ userId: req.params.userId });
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
